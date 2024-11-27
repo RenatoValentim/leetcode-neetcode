03b1ef34c5da97fbc27e29fa758945ae4db94ac7
Renato Valentim <48966099+RenatoValentim@users.noreply.github.com>
2024-11-27 09:22:12 -0300

    feat(arrayshashing): should return false if has an empty string

---
+++ internal/arrayshashing/validanagram.go
@@ -0,0 +1,8 @@
+package arrayshashing
+
+func IsAnagram(input1 string, input2 string) bool {
+  if input1 == "" || input2 == "" {
+    return false
+  }
+  return false
+}
---
+++ internal/arrayshashing/validanagram_test.go
@@ -0,0 +1,32 @@
+package arrayshashing_test
+
+import (
+  "testing"
+  "github.com/RenatoValentim/leetcode-neetcode/internal/arrayshashing"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestValidAnagram(t *testing.T) {
+  assert := assert.New(t)
+
+  testCases := []struct {
+    name string
+    input1 string
+    input2 string
+    expected bool
+  } {
+      {
+        name: "Should return false when providing any empty string",
+        input1: "",
+        input2: "",
+        expected: false,
+      },
+  }
+
+  for _, tc := range testCases {
+    t.Run(tc.name, func(t *testing.T) {
+      output := arrayshashing.IsAnagram(tc.input1, tc.input2)
+      assert.Equal(tc.expected, output)
+    })
+  }
+}
