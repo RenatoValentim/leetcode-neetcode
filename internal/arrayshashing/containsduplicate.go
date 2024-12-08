3e132602df200d83211858a6c78e185f4edcbab7
Renato Valentim <48966099+RenatoValentim@users.noreply.github.com>
2024-12-08 11:40:42 -0300

    refactor(arrayshashing): simplify logic

--- internal/arrayshashing/containsduplicate.go
+++ internal/arrayshashing/containsduplicate.go
@@ -3,19 +3,16 @@ package arrayshashing
 type Set map[int]bool
 
 func (s Set) add(value int) {
-  s[value] = true
+	s[value] = true
 }
 
 func HasDuplicated(values []int) bool {
-  if len(values) == 0 {
-    return false
-  }
-  s := make(Set)
-  for _, val := range values {
-    s.add(val)
-  }
-  if len(values) != len(s) {
-    return true
-  }
-  return false
+	if len(values) == 0 {
+		return false
+	}
+	s := make(Set)
+	for _, val := range values {
+		s.add(val)
+	}
+	return len(values) != len(s)
 }
